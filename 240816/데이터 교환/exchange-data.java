e229cddfc4e681663da9d08ec7bf3a94b54e0818
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-08-16 17:00:39 +0900

    Title: 데이터 교환, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240816/데이터 교환/exchange-data.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int tmp = 0;
+
+        tmp = b;
+        b = a;
+        a = c;
+        c = tmp;
+
+        System.out.println(a+"\n"+b+"\n"+c);
+
+    }
+}
