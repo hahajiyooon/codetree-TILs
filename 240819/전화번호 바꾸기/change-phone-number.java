f3df42b47c1271ba5e6cf2f2a299ef715b69abec
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-08-19 18:00:11 +0900

    Title: 전화번호 바꾸기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240819/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        sc.nextInt();
+        int xxxx = sc.nextInt();
+        int yyyy = sc.nextInt();
+
+        System.out.print("010-"+yyyy+"-"+xxxx);
+    }
+}
