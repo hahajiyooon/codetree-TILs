3167a81fb525ccf4117fbb20f0237e0f03ad09ae
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-08-19 17:36:58 +0900

    Title: 입력받아 출력 2, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240819/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+
+        System.out.println(sc.nextInt()+""+sc.nextInt());
+    }
+}
