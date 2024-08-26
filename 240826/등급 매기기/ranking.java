4cd7ef51c887e1b388f93f17bb1218d4b548e828
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-08-26 15:50:40 +0900

    Title: 등급 매기기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240826/등급 매기기/ranking.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        
+        switch(score/10){
+            case 10 : System.out.println("A"); break;
+            case 9 : System.out.println("A"); break;
+            case 8 : System.out.println("B"); break;
+            case 7 : System.out.println("C"); break;
+            case 6 : System.out.println("D"); break;
+            default : System.out.println("F");
+        }
+
+        
+        
+    }
+}
