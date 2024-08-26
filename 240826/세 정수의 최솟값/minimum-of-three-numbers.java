a2ebe0d51069adcf48485a094f416c5c53cc5670
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-08-26 17:38:19 +0900

    Title: 세 정수의 최솟값, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240826/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int min = a;
+
+        if(min > b){ min = b;}
+        if(min > c){ min = c;}
+
+        System.out.println(min);
+    }
+}
