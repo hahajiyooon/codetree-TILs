87ce6623324aae78c4a4ecaf5f55864e511a2de5
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-08-27 16:58:56 +0900

    Title: 배수의 개수, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240827/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        int cnt3 = 0; 
+        int cnt5 = 0;
+
+        for(int i=0; i<10; i++){
+            n = sc.nextInt();
+            if(n%3 == 0) cnt3++;
+            if(n%5 == 0) cnt5++;
+        }
+        System.out.print(cnt3+" "+cnt5);
+    }
+}
