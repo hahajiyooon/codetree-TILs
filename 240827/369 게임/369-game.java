657e654285a651ada42cea2d6b85f315bbabcdfd
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-08-27 16:04:07 +0900

    Title: 369 게임, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240827/369 게임/369-game.java
+++ 240827/369 게임/369-game.java
@@ -5,18 +5,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n =sc.nextInt();
         int c = 0;
+        boolean flag ; 
 
         for(int i=1; i<=n; i++){
             c = i;
+            flag = false;
             if(c%3==0) {
-                System.out.print(0+" "); continue;
+                flag = true;
+            }else{
+                while(c>0){
+                    if(c%10 != 0 && c%10%3 == 0){
+                        flag = true; break;
+                    }
+                    c /= 10;
+                }
             }
-
-            while(c != 0){
-                if(c%10%3 == 0) {System.out.print(0+" "); break;}
-                c /= 10;
-            }
-            System.out.print(i+" ");
+            System.out.print(flag == true? 0+" " : i+" ");
         }
     }
 }
