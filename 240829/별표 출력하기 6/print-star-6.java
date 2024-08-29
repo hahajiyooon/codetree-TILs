164dd3d3519543fce0d2578bfa064dc2a330be09
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-08-29 15:27:47 +0900

    Title: 별표 출력하기 6, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240829/별표 출력하기 6/print-star-6.java
+++ 240829/별표 출력하기 6/print-star-6.java
@@ -7,7 +7,7 @@ public class Main {
 
         for(int i=0; i<n; i++){
             for(int j=0; j<i; j++){
-                System.out.print("vv");
+                System.out.print("  ");
             }
 
             for(int j=0; j<2*n-2*i-1; j++){
