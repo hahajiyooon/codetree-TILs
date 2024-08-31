283a1b60ecc5d373524cbd489895b9d8130cd91f
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-08-31 14:36:42 +0900

    Title: 별표 출력하기 12, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240831/별표 출력하기 12/print-star-12.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        //짝, 홀에 따른 별 구성 체크
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 || j%2 == 0 && j>=i){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
