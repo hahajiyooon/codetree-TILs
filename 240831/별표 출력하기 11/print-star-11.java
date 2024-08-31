cf0881a92de9beb513ea2d0cb3f0d6ea30190288
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-08-31 14:03:13 +0900

    Title: 별표 출력하기 11, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240831/별표 출력하기 11/print-star-11.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        //n=1 > 3 2n+1 and i%2==0인경우에 *
+        
+        
+        for(int i=0; i<2*n+1; i++){
+            for(int j=0; j<2*n+1; j++){
+                if(i%2==0 || j%2==0){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
