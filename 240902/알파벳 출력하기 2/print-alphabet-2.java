60064050ee327600cc707b8f82539882c10921b0
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-09-02 14:55:47 +0900

    Title: 알파벳 출력하기 2, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240902/알파벳 출력하기 2/print-alphabet-2.java
+++ 240902/알파벳 출력하기 2/print-alphabet-2.java
@@ -9,6 +9,7 @@ public class Main {
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 System.out.print((i > j)? "  " : c++ +" ");
+                if(c >'Z') c = 'A';
             }
             System.out.println();
         }
