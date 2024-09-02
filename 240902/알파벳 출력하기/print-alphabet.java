699198894aa5563efbbb362597dd3aad372d8fb1
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-09-02 14:40:09 +0900

    Title: 알파벳 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240902/알파벳 출력하기/print-alphabet.java
+++ 240902/알파벳 출력하기/print-alphabet.java
@@ -9,6 +9,7 @@ public class Main {
         for(int i=0; i<n; i++){
             for(int j=0; j<i+1; j++){
                 System.out.print((char)(c++));
+                if((char)c = 'Z') c ='A';
             }
             System.out.println();
         }
