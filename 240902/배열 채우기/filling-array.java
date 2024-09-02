c0dc3ab615b26a6c35cb86cadda2f830b87e3b77
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-09-02 16:56:01 +0900

    Title: 배열 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240902/배열 채우기/filling-array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int n = 0;
+        int idx = 0;
+
+        while((n = sc.nextInt()) != 0){
+            arr[idx++] = n;
+        }
+
+        for(int i=9; i>-1; i--){
+            if(arr[i] != 0) System.out.print(arr[i]+" ");
+        }
+
+    }
+}
