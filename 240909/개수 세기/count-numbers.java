90f9c8e52d47bc41435c68ea34a0c5755f78d952
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-09-09 15:07:10 +0900

    Title: 개수 세기, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240909/개수 세기/count-numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int cnt = 0;
+
+        for(int i=0; i<n; i++){
+            if(m == sc.nextInt()) cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
