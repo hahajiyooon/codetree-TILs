aff9b366b72cfbdf863b9567808ece44f476de3e
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-09-10 15:50:56 +0900

    Title: 두 개의 격자 비교하기, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240910/두 개의 격자 비교하기/compare-two-grid.java
+++ 240910/두 개의 격자 비교하기/compare-two-grid.java
@@ -10,18 +10,18 @@ public class Main {
         int[][] b = new int[n][m];
 
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+            for(int j=0; j<m; j++){
                 a[i][j] = sc.nextInt();
             }
         }
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+            for(int j=0; j<m; j++){
                 b[i][j] = sc.nextInt();
             }
         }
 
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+            for(int j=0; j<m; j++){
                 System.out.print(a[i][j] == b[i][j]? 0 : 1);
                 System.out.print(" ");
             }
