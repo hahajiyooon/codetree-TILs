91abf0e83ea7234ff44d317de277d642ad8444ec
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-09-16 16:37:08 +0900

    Title: 미는 횟수, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240916/미는 횟수/number-of-pushes.java
+++ 240916/미는 횟수/number-of-pushes.java
@@ -5,16 +5,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         String b = sc.next();
+        int leng = a.length();
         int cnt = 0;
         boolean match = false;
 
-        for(int i=0; i<=a.length(); i++){
+        for(int i=0; i<=leng; i++){
             if(a.equals(b)) {
                 match = true;
                 break;
             }
 
-            a = a.substring(1)+a.substring(0,1);
+            a = a.substring(leng-1)+a.substring(0,leng-1);
             cnt++;
         }
 
