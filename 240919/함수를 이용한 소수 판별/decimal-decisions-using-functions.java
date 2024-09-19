e94b80f626210e90e027229766dd3e817ccaba6b
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-09-19 17:56:30 +0900

    Title: 함수를 이용한 소수 판별, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240919/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static int sum(int a, int b){
+        boolean decimal = true;
+        int s = 0;
+
+        for(int i=a; i<=b; i++){
+            decimal = true;
+            
+            for(int j=2; j<i; j++){
+                if(i%j == 0) {
+                    decimal = false; 
+                    break;
+                }
+            }
+            if(decimal) s += i;
+        }
+
+        return s;
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(sum(a,b));
+    }
+}
