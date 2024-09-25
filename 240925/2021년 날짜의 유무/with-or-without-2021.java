df8aa10f804e248d774208e374585d8e2851ebe9
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-09-25 15:32:58 +0900

    Title: 2021년 날짜의 유무, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240925/2021년 날짜의 유무/with-or-without-2021.java
+++ 240925/2021년 날짜의 유무/with-or-without-2021.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static boolean isDate(int m, int d){
         int[] mArray={1,2,3,4,5,6,7,8,9,10,11,12};
-        int[] dArray={31,28,31,30,31,30,31,30,31,30,31};
+        int[] dArray={31,28,31,30,31,30,31,31,30,31,30,31};
 
         for(int i=0; i<12; i++){
             if(mArray[i] == m) {
