22d537c0205e7382b6b226a329601682c7e8f0b3
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-09-25 15:30:06 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240925/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static boolean isDate(int m, int d){
+        int[] mArray={1,2,3,4,5,6,7,8,9,10,11,12};
+        int[] dArray={31,28,31,30,31,30,31,30,31,30,31};
+
+        if(dArray[m-1] < d) return false;
+
+        for(int i=0; i<12; i++){
+            if(mArray[i] == m) {
+                if(dArray[i] >= d){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+
+        System.out.print(isDate(m,d)? "Yes" : "No");
+    }
+}
