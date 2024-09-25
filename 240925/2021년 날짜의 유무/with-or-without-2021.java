2c05730376d7e974a9e48643fbc1c7e3ce57fc26
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-09-25 15:30:42 +0900

    Title: 2021년 날짜의 유무, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240925/2021년 날짜의 유무/with-or-without-2021.java
+++ 240925/2021년 날짜의 유무/with-or-without-2021.java
@@ -5,8 +5,6 @@ public class Main {
         int[] mArray={1,2,3,4,5,6,7,8,9,10,11,12};
         int[] dArray={31,28,31,30,31,30,31,30,31,30,31};
 
-        if(dArray[m-1] < d) return false;
-
         for(int i=0; i<12; i++){
             if(mArray[i] == m) {
                 if(dArray[i] >= d){
