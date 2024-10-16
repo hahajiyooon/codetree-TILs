cfe99068a7efd28088f76eb7af49f48c4503944f
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-10-16 15:47:56 +0900

    Title: 사는 지역, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241016/사는 지역/where-live.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Person{
+    String name = "";
+    String address = "";
+    String area = "";
+
+    public Person(){
+
+    }
+
+    public Person(String name, String address, String area){
+        this.name = name;
+        this.address = address;
+        this.area = area;
+    }
+}
+
+public class Main {
+    public static String lastName ;
+
+    //사전순으로 이름이 가장 느린 사람
+    public static boolean TakeName(String name){
+        int minLength = (lastName.length() < name.length())? lastName.length() : name.length();
+
+        for(int i=0; i<minLength; i++){
+            if(name.charAt(i) > lastName.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String name, addr, city;
+
+        Person person = new Person();
+
+        for(int i=0; i<n; i++){
+            name = sc.next();
+            addr = sc.next();
+            city = sc.next();
+
+            if(i == 0){
+                lastName = name;
+                person = new Person(name,addr,city);
+            }else if(TakeName(name)){
+                person = new Person(name,addr,city);
+            }
+        }//for
+
+        System.out.println("name "+person.name);
+        System.out.println("addr "+person.address);
+        System.out.println("city "+person.area);
+    }
+}
