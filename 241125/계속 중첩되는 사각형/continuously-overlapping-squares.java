a7514af97aae1aa76591a7c8fc6f2a2751e81d32
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-11-25 14:48:50 +0900

    Title: 계속 중첩되는 사각형, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241125/계속 중첩되는 사각형/continuously-overlapping-squares.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+    public static final int offset = 100;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 0;
+        boolean flag = false;
+        boolean[][] square = new boolean[201][201];
+
+        for(int i=0; i<n; i++){
+            x1 = sc.nextInt()+offset;
+            y1 = sc.nextInt()+offset;
+            x2 = sc.nextInt()+offset;
+            y2 = sc.nextInt()+offset;
+
+            if(i%2 == 0){
+                flag = false;
+                for(int x=x1; x<x2; x++){
+                    for(int y=y1; y<y2; y++){
+                        square[x][y] = flag;
+                    }
+                }
+            }else{
+                flag = true;
+                for(int x=x1; x<x2; x++){
+                    for(int y=y1; y<y2; y++){
+                        square[x][y] = flag;
+                    }
+                }
+
+            }
+        }//while
+
+        int cnt = 0;
+        for(int x=x1; x<x2; x++){
+            for(int y=y1; y<y2; y++){
+                if(square[x][y]) cnt++;
+            }
+        }
+        
+        System.out.print(cnt);
+
+    }
+}
