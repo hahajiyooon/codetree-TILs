ad58a72b89e1393fe8df61f83f0154e63d52df52
hajiyoon <114209707+hahajiyooon@users.noreply.github.com>
2024-11-25 11:11:15 +0900

    Title: 색종이의 총 넓이, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241125/색종이의 총 넓이/the-total-area-of-colored-paper.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static final int offset = 100; //좌표 보정상수
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int x = 0;
+        int y = 0; //좌측하단 점
+        int cnt = 0;
+        boolean [][] coloredPapaer = new boolean[201][201];
+
+        for(int i=0; i<n; i++){
+            x = sc.nextInt();
+            y = sc.nextInt();
+
+            for(int a=0; a<x+8; a++){
+                for(int b=0; b<y+8; b++){
+                    coloredPapaer[a][b] = true;
+                }
+            }
+        }//for
+
+        for(int i=0; i<201; i++){
+            for(int j=0; j<201; j++){
+                if(coloredPapaer[i][j]) cnt++;
+            }
+        }
+        
+        System.out.print(cnt);
+
+    }
+}
